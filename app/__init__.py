6d49bab142f6fbe6ba5ae14a474c6210772d4858
Fai <chepngetichfaith46@gmail.com>
2021-06-05 15:00:31 +0300

    add configuration settings to init file and remove debug true from run file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
+from app.config import DevConfig
 from flask import Flask
 
 #initializing application
 app = Flask(__name__)
 
+#setting up configuration
+app.config.from_object(DevConfig)
 from app import views
