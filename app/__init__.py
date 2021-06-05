c6909badf0c07b341a301e11082c7c74ad172101
Fai <chepngetichfaith46@gmail.com>
2021-06-05 15:27:00 +0300

    add api key and place it in gitignore file

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
-from app.config import DevConfig
+from .config import DevConfig
 from flask import Flask
 
 #initializing application
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
 
 #setting up configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 from app import views
