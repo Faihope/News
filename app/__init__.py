87ea36d1ccc55cb31bc87b1fc642fa84ff24cfaf
Fai <chepngetichfaith46@gmail.com>
2021-06-07 01:45:54 +0300

    Add bootstrap,macro and base html

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from .config import DevConfig
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 #initializing application
 app = Flask(__name__,instance_relative_config = True)
@@ -7,4 +8,6 @@ app = Flask(__name__,instance_relative_config = True)
 #setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 from app import views
